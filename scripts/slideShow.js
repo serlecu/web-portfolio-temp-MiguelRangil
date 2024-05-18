798aaa0f800e47a982d386cf92ddc360c8f146e0
Sergio Lecuona <serlecu@hotmail.com>
2024-05-18 18:40:34 +0200

    slideshow for projects

---
+++ scripts/slideShow.js
@@ -0,0 +1,23 @@
+
+function slideShow() {
+  console.log("running slideShow");
+  let anyActive = false;
+  const slideShow = document.getElementsByClassName("proj-img-slideshow");
+  // const pics = slideShow[0].getElementsByTagName("img");
+  const pics = slideShow[0].children;
+
+  for (i = 0; i < pics.length; i++) {
+    if (pics[i].className === "show"){
+      anyActive = true;
+      pics[i].className = "hidden";
+      i == pics.length - 1 ? pics[0].className = "show" : pics[i+1].className = "show";
+      break;
+    }
+  }
+
+  if (!anyActive){
+    for (i = 0; i < pics.length; i++) {
+      i == 0 ? pics[i].className = "show" : pics[i].className = "hidden";
+    }
+  }
+}
